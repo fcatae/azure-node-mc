c64d0bead611b38e1220ba202d58bfa5122bef4a
Fabricio Catae <fabricio.catae@microsoft.com>
2016-02-14 21:58:41 -0200

    replaced with flying-squid

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-var http = require('http');
+// var http = require('http');
+// 
+// var port = process.env.PORT || 1337;
+// 
+// http.createServer(app).listen(port);
+// 
+// function app(req,res) {
+//     res.end('hello world! I am listening at ' + port);
+// }
+// 
+// console.log('hello world')
 
-var port = process.env.PORT || 1337;
+var mcServer = require("flying-squid");
 
-http.createServer(app).listen(port);
+mcServer.createMCServer({
+  "motd": "A Minecraft Server \nRunning flying-squid",
+  "port": process.env.PORT || 1337,
+  "max-players": 10,
+  "online-mode": true,
+  "logging": true,
+  "gameMode": 1,
+  "generation": {
+    "name": "diamond_square",
+    "options":{
+      "worldHeight": 80
+    }
+  },
+  "kickTimeout": 10000,
+  "plugins": {
 
-function app(req,res) {
-    res.end('hello world! I am listening at ' + port);
-}
-
-console.log('hello world')
+  },
+  "modpe": false,
+  "view-distance": 10
+});
